e58d0eb9f49114e9c7a1ef175a6387ceba01f56d
Klaas tom Dieck <klaas.tom-dieck@campus.tu-berlin.de>
2026-06-11 11:00:49 +0200

    chore(clang-tidy): LogicalPlanBuilder.cpp

--- nes-logical-operators/src/Plans/LogicalPlanBuilder.cpp
+++ nes-logical-operators/src/Plans/LogicalPlanBuilder.cpp
@@ -49,6 +49,7 @@
 #include <WindowTypes/Types/TimeBasedWindowType.hpp>
 #include <WindowTypes/Types/WindowType.hpp>
 #include <ErrorHandling.hpp>
+#include "DataTypes/TimeUnit.hpp"
 
 namespace NES
 {
